bc0fae75f5639d6ae5ba8a6372977aa94d985491
Ping-Ju, Chen <cpr860632231@gmail.com>
2023-02-26 13:57:56 -0500

    update to step2

---
+++ components/MainNav.js
@@ -0,0 +1,41 @@
+import { Container, Nav, Navbar, Form, Button } from "react-bootstrap";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+function MainNav() {
+  return (
+    <>
+      <Navbar bg="dark" expand="lg" variant="dark" className="fixed-top">
+        <Container>
+          <Navbar.Brand>PingJu Chen</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link href="/" passHref legacyBehavior>
+                <Nav.Link>Home</Nav.Link>
+              </Link>
+              <Link href="/search" passHref legacyBehavior>
+                <Nav.Link>Advanced Search</Nav.Link>
+              </Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success" type="submit">
+                Search
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <br />
+      <br />
+    </>
+  );
+}
+
+export default MainNav;
