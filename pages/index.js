9daf7814d92dab7bc5c376bf6b482b082b1a93f1
Ping-Ju, Chen <cpr860632231@gmail.com>
2023-03-16 11:52:38 -0400

    updated header

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
- * WEB422 – Assignment 4
+ * WEB422 – Assignment 5
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  * No part of this assignment has been copied manually or electronically from any other source
  * (including web sites) or distributed to other students.
  *
- * Name: PingJu Chen   Student ID: 151043205   Date: 2023-03-07
+ * Name: PingJu Chen   Student ID: 151043205   Date: 2023-03-16
  *
  ********************************************************************************/
 import Image from "react-bootstrap/Image";
