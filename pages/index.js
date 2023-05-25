ad3dbcb4a4c3619a71b44311ce31fe7fea01de05
Ping-Ju Chen <103694860+AmyRChen@users.noreply.github.com>
2023-05-24 21:01:39 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-/*********************************************************************************
- * WEB422 – Assignment 6
- * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- * No part of this assignment has been copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
- *
- * Name: PingJu Chen   Student ID: 151043205   Date: 2023-04-03
- *
- * Vercel App (Deployed) Link: https://web-422-a6-client.vercel.app/
- *
- ********************************************************************************/
 import Image from "react-bootstrap/Image";
 import { Row, Col } from "react-bootstrap";
 
